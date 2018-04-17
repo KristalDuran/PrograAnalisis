11642c238a7256c4297ea509eb63c3781b4f22ae
Curso <curso@HA-12.ec.tec.ac.cr>
2018-04-17 14:20:41 -0600

    prueba
    print Holamundo

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -24,6 +24,6 @@ public class Main {
         menu.fijarControlador(new ControllerMenu(menu, graphMethods));
         
         menu.fijarModelo(graphMethods);
-        
+        System.out.println("holaMundo");
     }
 }
